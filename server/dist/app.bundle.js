20ba392b91089b5b881830e3efdd9f6a298482c1
li-zq <li-zq@neusoft.com>
2017-07-08 13:45:27 +0800

    7.8

--- server/dist/app.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],{2:function(o,a,n){"use strict";(function(o){o.ajax({url:"/api",type:"GET",dataType:"json",data:{param1:"value1",param2:"value2"}}).done(function(){console.log("success")}).fail(function(){console.log("error")}).always(function(){console.log("complete")})}).call(a,n(0))}},[2]);
