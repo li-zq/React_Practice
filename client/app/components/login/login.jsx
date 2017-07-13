d2e25137517e41b9cf4195f99e15af7f26268fe7
li-zq <lizq152103@gmail.com>
2017-07-13 23:08:02 +0800

    7.13

--- client/app/components/login/login.jsx
+++ client/app/components/login/login.jsx
@@ -1,13 +1,43 @@
 import "./login.less";
 
+import loginImg from "./image/login.jpg" 
+console.log(loginImg)
 export default class Login extends React.Component {
 	constructor(props){
 		super(props);
+		this.handel = this.handel.bind(this);
+	}
+	handel(){
+		console.log(1);
 	}
 	render(){
 		return (
-			<div>
-				{'login'}
+			<div className="content">
+				<form name="loginForm" role="form" noValidate>
+				    <div className="boxinput">
+				        <div className="user-input">
+				            <input autoFocus="autofocus" type="text" name="username"  placeholder="手机号" required/>
+				            <span className="user">
+				            	<span className="checkUser place"></span>
+				            </span>
+				        </div>
+				        <div className="password-input">
+				           <input type="password" name="password"  placeholder="请输入您的密码" required/>
+			            	<span className="passwordClick">
+				            	<span className="checkPasssWord place"></span>
+				            </span>
+				        </div>
+				        <div>
+					        <button type="submit" >
+					        </button>
+					    </div>
+						<div className="rememberPassword">
+					    	<span className="checkbox"></span>
+					      <span><a href="javascript:void(0);">记住账户</a></span>
+					      <span><a href="javascript:void(0);">忘记密码？</a></span>
+					    </div>
+				    </div>
+				</form>
 			</div>	
 		)
 	}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -58,6 +58,9 @@ module.exports = {
 			},{
 				test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         		use: 'file-loader'
+			},{
+				test: /\.(gif|png|jpg)$/,
+				use: 'file-loader?name=img-[name].[ext]'
 			}
 		]
 	},
